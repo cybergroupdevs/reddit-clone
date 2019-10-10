bc191c6113ce3ed36ccecffca048278e91d9b46c
Ganga Singh <ganga.singh@cybergindia.local>
2019-10-11 02:08:06 +0530

    added routes for signIn

--- rest-apis-express/router/apiRouter.js
+++ rest-apis-express/router/apiRouter.js
@@ -4,13 +4,21 @@ const { baseURI } = require("../config").config;
 
 const { users } = require("../controllers")
 
+const { signIn } = require ("../controllers");
+
+const auth  = require("../auth/authenticator");
+
 module.exports = () => {
   const router = Router();
+  router.get(`${baseURI}/signIn/users`, users.getUsers);
+  router.post(`${baseURI}/signIn/users`, users.createUser);
+  router.patch(`${baseURI}/signIn/users`, users.updateUser);
+  router.delete(`${baseURI}/signIn/users`, users.deleteUser);
 
-  router.get(`${baseURI}/users`, users.getUsers);
-  router.post(`${baseURI}/users`, users.createUser);
-  router.patch(`${baseURI}/users`, users.updateUser);
-  router.delete(`${baseURI}/users`, users.deleteUser);
+  router.get(`${baseURI}/signIn`, signIn.getUsers);
+  router.post(`${baseURI}/signIn`, signIn.createUsers);
+  router.patch(`${baseURI}/signIn`, signIn.updateUsers);
+  router.delete(`${baseURI}/signIn`, signIn.deleteUsers);
 
   return router;
 }
