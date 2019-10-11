c56e98da2ee8f284feef8dda4f061e6bc74146f4
Chandan <ch.cse16@chitkarauniversity.edu.in>
2019-10-11 09:31:39 +0530

    change the name of controller and model

--- rest-apis-express/controllers/users.js
+++ rest-apis-express/controllers/users.js
@@ -1,52 +1,52 @@
-const { user } = require("../models");
+// const { user } = require("../models");
 
-module.exports = {
-  getUsers,
-  createUser,
-  updateUser,
-  deleteUser,
-};
+// module.exports = {
+//   getUsers,
+//   createUser,
+//   updateUser,
+//   deleteUser,
+// };
 
-  async function getUsers(req, res) {
-  const response = await user.getUsers();
-  res.send(response);
-  }
+//   async function getUsers(req, res) {
+//   const response = await user.getUsers();
+//   res.send(response);
+//   }
 
-function createUser(req, res) {
-  const body = req.body;
-  users.push(body);
+// function createUser(req, res) {
+//   const body = req.body;
+//   users.push(body);
 
-  res.send({
-    status: 200,
-    statusText: "OK",
-    message: "Client Inserted!"
-  });
-}
+//   res.send({
+//     status: 200,
+//     statusText: "OK",
+//     message: "Client Inserted!"
+//   });
+// }
 
-function updateUser(req, res) {
-  const body = req.body;
-  const id = req.query.id;
-}
-  console.log(id);
+// function updateUser(req, res) {
+//   const body = req.body;
+//   const id = req.query.id;
+// }
+//   console.log(id);
 
-  for (let key in body) {
-    users[id][key] = body[key];
-  }
+//   for (let key in body) {
+//     users[id][key] = body[key];
+//   }
 
-  res.send({
-    status: 200,
-    statusText: "OK",
-    message: "Client Updated!"
-  });
+//   res.send({
+//     status: 200,
+//     statusText: "OK",
+//     message: "Client Updated!"
+//   });
 
-function deleteUser(req, res) {
-  const id = req.query.id;
+// function deleteUser(req, res) {
+//   const id = req.query.id;
 
-  users.pop(id);
+//   users.pop(id);
 
-  res.send({
-    status: 200,
-    statusText: "OK",
-    message: "Client Deleted!"
-  });
-}
+//   res.send({
+//     status: 200,
+//     statusText: "OK",
+//     message: "Client Deleted!"
+//   });
+// }
