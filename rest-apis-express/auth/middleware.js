a64881b01f825e4597b7725f02425b22a70ad414
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2019-10-15 15:34:29 +0530

    token validation middleware

---
+++ rest-apis-express/auth/middleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const { SECRET } = require('../config').config;
+
+module.exports= isAuthenticated = (req,res,next)=> {
+    const token = req.headers.token;
+        jwt.verify(token,new Buffer(SECRET, 'base64'), (err) => {
+            if(err){
+              
+                res.send({"message": "session expired"})
+             
+            }else {
+              next() //TODO: hit the next user profile route
+            }
+         
+        });
+  }
