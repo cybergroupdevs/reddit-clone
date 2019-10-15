b11bb659d1e28b38e8c2d084ff576e96927cb947
Lakshya Kalia <lkalia.cse16@chitkarauniversity.edu.in>
2019-10-15 18:53:54 +0530

    Conflicts resoved-3

--- rest-apis-express/models/usercomments.js
+++ rest-apis-express/models/usercomments.js
@@ -22,52 +22,44 @@ async function getUsers(req) {
 
 async function createUser(req, res) {
     let response;
-    let body, details; <<
-    << << < HEAD
-    body = req.body;
-    details = new info(body)
+    let body, details;
+    // body=req.body;
+    // details= new info(body)
     console.log(details)
+    debugger
     try {
-        response = await details.save() ===
-            === =
-            // body=req.body;
-            // details= new info(body)
-            console.log(details)
-        debugger
-        try {
-            response = await info.create(req.body); >>>
-            >>> > 3014259 c0f2ed379ddca069e89c608efbee52caf
-            return response
+        response = await info.create(req.body);
+        return response
 
-        } catch (err) {
-            response = { error: err }
-            return response
-        }
-
+    } catch (err) {
+        response = { error: err }
+        return response
     }
 
-    async function updateUser(req, res) {
-        const body = req.body;
-        const _id = req.query.id;
-        // console.log(id);
-        console.log(body);
-        await info.findByIdAndUpdate(_id, body)
-        return ({
-            status: 200,
-            statusText: "OK",
-            message: "Client Updated!"
-        });
-    }
+}
 
-    async function deleteUser(req, res) {
-        const id = req.query.id;
-        console.log(id);
-        await info.findByIdAndDelete(id);
+async function updateUser(req, res) {
+    const body = req.body;
+    const _id = req.query.id;
+    // console.log(id);
+    console.log(body);
+    await info.findByIdAndUpdate(_id, body)
+    return ({
+        status: 200,
+        statusText: "OK",
+        message: "Client Updated!"
+    });
+}
 
-        res.send({
-            status: 200,
-            statusText: "OK",
-            message: "Client deleted!"
-        });
+async function deleteUser(req, res) {
+    const id = req.query.id;
+    console.log(id);
+    await info.findByIdAndDelete(id);
 
-    }
+    res.send({
+        status: 200,
+        statusText: "OK",
+        message: "Client deleted!"
+    });
+
+}
