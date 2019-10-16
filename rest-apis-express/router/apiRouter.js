e55e9e2a628b9bc3138b790f6be830f3e02062a4
Ashish Rawat <ashish.rawat@cybergindia.local>
2019-10-16 11:40:49 +0530

    conflict resolved of route file

--- rest-apis-express/router/apiRouter.js
+++ rest-apis-express/router/apiRouter.js
@@ -5,11 +5,6 @@ const { baseURI } = require("../config").config;
 
 const api = require("../controllers");
 
-<<<<<<< HEAD
-const middleware = require("../auth/middleware");
-
-const { userscomments } = require("../controllers");
-=======
 const { signIn } = require("../controllers");
 
 const middleware = require("../auth/middleware");
@@ -18,94 +13,35 @@ const post = require("../controllers").post;
 
 const { userscomments } = require("../controllers");
 
->>>>>>> aa44110f54978cebc77539531802e35b84f40275
 const { users } = require("../controllers");
 
 const createToken = require("../auth/authenticator").checkAuth;
 
 module.exports = () => {
-<<<<<<< HEAD
-  var app = express()
-  
-
-
-  app.post(`${baseURI}/signIn`, async function (req, res) {
-    const result = await createToken(req)
-    res.send(result);
-  });
-
-  app.post(`${baseURI}/signUp`, async function (req, res) {
-    const response = await api.signIn.createUsers(req)
-    res.send(response)
-  });
-
-  app.post(`${baseURI}/testToken`, middleware, async function (req, res) {
-    const result = ({ "message": "valid" })
-    res.send(result);
-  });
-
-  app.get(`${baseURI}/comments`, middleware, async function (req, res) {
-
-    const result = await userscomments.getUsers(req, res);
-    console.log(result)
-    res.send(result);
-  });
-
-  app.post(`${baseURI}/comments`, middleware, async function (req, res) {
-    const result = userscomments.createUsers(req, res)
-    res.send(result);
-  });
-
-  app.patch(`${baseURI}/comments`, middleware, async function (req, res) {
-    const result = userscomments.updateUsers(req, res)
-    res.send(result);
-  });
-
-  app.delete(`${baseURI}/comments`, middleware, async function (req, res) {
-    const result = userscomments.deleteUsers(req, res)
-    res.send(result);
-  });
-
-  //////
-  app.get(`${baseURI}/users`, middleware, async function (req, res) {
-
-    const result =  users.getUsers(req, res);
-    console.log(result)
-    res.send(result);
-  });
-
-  app.post(`${baseURI}/users`, middleware, async function (req, res) {
-    const result = users.createUsers(req, res)
-    res.send(result);
-  });
-
-  app.patch(`${baseURI}/users`, middleware, async function (req, res) {
-    const result = users.updateUsers(req, res)
-    res.send(result);
-  });
-
-  app.delete(`${baseURI}/users`, middleware, async function (req, res) {
-    const result = users.deleteUsers(req, res)
-    res.send(result);
-  });
-
-  // TODO: Furter enhancement
-  // app.patch(`${baseURI}/update`,function(req,res){   
-  //   res.send(api.signIn.updateUsers)
-  // });
-
-  // app.delete(`${baseURI}/delete`,function(req,res){   
-  //   res.send(api.signIn.deleteUsers)
-  // });
-
-  return app;
-=======
     var app = express()
-    app.get(`${baseURI}/users`, users.getUsers);
-    app.post(`${baseURI}/users`, users.createUser);
-    app.patch(`${baseURI}/users`, users.updateUser);
-    app.delete(`${baseURI}/users`, users.deleteUser);
+    
+    app.get(`${baseURI}/users`, middleware, async function (req, res) {
 
+      const result =  users.getUsers(req, res);
+      console.log(result)
+      res.send(result);
+    });
+  
+    app.post(`${baseURI}/users`, middleware, async function (req, res) {
+      const result = users.createUsers(req, res)
+      res.send(result);
+    });
+  
+    app.patch(`${baseURI}/users`, middleware, async function (req, res) {
+      const result = users.updateUsers(req, res)
+      res.send(result);
+    });
+  
+    app.delete(`${baseURI}/users`, middleware, async function (req, res) {
+      const result = users.deleteUsers(req, res)
+      res.send(result);
+    });
+  
 
     app.post(`${baseURI}/signIn`, async function(req, res) {
         const result = await createToken(req)
@@ -175,5 +111,4 @@ module.exports = () => {
     // });
 
     return app;
->>>>>>> aa44110f54978cebc77539531802e35b84f40275
 }
