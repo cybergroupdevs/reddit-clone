3543200416f7e0d65894ca373f9b58bc5362586c
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2019-10-17 17:27:08 +0530

    conflict resolved

--- rest-apis-express/router/apiRouter.js
+++ rest-apis-express/router/apiRouter.js
@@ -24,7 +24,6 @@ function apiRouter() {
         const result = await users.getUsers(req, res)
         res.send(result);
     });
-<<<<<<< HEAD
     
 
 app.post(`${baseURI}/users`, async function(req, res) {
@@ -115,94 +114,6 @@ app.post(`${baseURI}/image`, function(req, res) {
 // });
 
 return app;
-=======
-
-
-    app.post(`${baseURI}/users`, async function(req, res) {
-        const result = users.createUser(req, res)
-        res.send(result);
-    });
-
-    app.patch(`${baseURI}/users`, middleware, async function(req, res) {
-        const result = users.updateUsers(req, res)
-        res.send(result);
-    });
-
-    app.delete(`${baseURI}/users`, middleware, async function(req, res) {
-        const result = users.deleteUsers(req, res)
-        res.send(result);
-    });
-
-
-    app.post(`${baseURI}/signIn`, async function(req, res) {
-        const result = await createToken(req)
-        res.send(result);
-    });
-
-    app.post(`${baseURI}/signUp`, async function(req, res) {
-        const response = await api.signIn.createUsers(req)
-        res.send(response)
-    });
-
-    app.post(`${baseURI}/testToken`, middleware, async function(req, res) {
-        const result = ({ "message": "valid" })
-        res.send(result);
-    });
-
-    app.get(`${baseURI}/comments`, middleware, async function(req, res) {
-
-        const result = await userscomments.getUsers(req, res);
-        console.log(result)
-        res.send(result);
-    });
-
-    app.post(`${baseURI}/comments`, middleware, async function(req, res) {
-        const result = userscomments.createUsers(req, res)
-        res.send(result);
-    });
-
-    app.patch(`${baseURI}/comments`, middleware, async function(req, res) {
-        const result = userscomments.updateUsers(req, res)
-        res.send(result);
-    });
-
-    app.delete(`${baseURI}/comments`, middleware, async function(req, res) {
-        const result = userscomments.deleteUsers(req, res)
-        res.send(result);
-    });
-
-    app.get(`${baseURI}/posts`, async function(req, res) {
-        const result = await post.getUsers(req, res);
-
-        res.send(result);
-    });
-
-    app.post(`${baseURI}/posts`, middleware, async function(req, res) {
-        const result = post.createUser(req, res)
-        res.send(result);
-    });
-
-    app.patch(`${baseURI}/posts`, middleware, async function(req, res) {
-        const result = post.updateUsers(req, res)
-        res.send(result);
-    });
-
-    app.delete(`${baseURI}/posts`, middleware, async function(req, res) {
-        const result = post.deleteUsers(req, res)
-        res.send(result);
-    });
-
-    // TODO: Furter enhancement
-    // app.patch(`${baseURI}/update`,function(req,res){   
-    //   res.send(api.signIn.updateUsers)
-    // });
-
-    // app.delete(`${baseURI}/delete`,function(req,res){   
-    //   res.send(api.signIn.deleteUsers)
-    // });
-
-    return app;
->>>>>>> 9f9dbd28adee7afd8f5a5e6120fd789ac39c8808
 }
 
 module.exports = {
