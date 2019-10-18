763ce957f19193cfd372691f71e9bbb80af5e3ff
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2019-10-18 13:44:41 +0530

    schema changed and post create functionality updated

--- rest-apis-express/schema/postSchema.js
+++ rest-apis-express/schema/postSchema.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
 const User = new mongoose.Schema({
-   user_id: String,
-   subreddits: [{
-       sub_id: String
-   }]
-
+    user_id: String,
+    subreddit_user_id: String,
+    subreddit_id: String,
 });
 const postModel = mongoose.model("userPostInfo", User);
 module.exports = {
